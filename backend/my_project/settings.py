f99ab17c30e68931bee7760e3366bb60b3af4fd5
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-11-01 16:49:37 -0400

    Update settings.py

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*7!!kc@bmtx8ngui6lr@xmifmcwm6y%hnbe)rdei(b!ds8t)uq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
